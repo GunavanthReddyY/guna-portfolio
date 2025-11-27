0ab5d3b824c231ad7f987a9524f538aaff658ca8
GunavanthReddyY <Gunavanthreddy06@gmail.com>
2025-11-27 07:23:35 +0530

    Fix router base for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // Change this if your repo name is different
-const repoName = "guna-portfolio";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/`,
+  base: '/',          // or simply remove `base` completely
+  build: {
+    outDir: 'dist',   // default, but safe to be explicit
+  },
 });
